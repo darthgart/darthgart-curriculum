cf38853cd601f3849000a4809e49a4b803b66ef7
Edgar Sanchez Gimenez <edsagi99@gmail.com>
2025-07-06 22:07:00 +0200

    update project - works and content

---
+++ src/utils/works.ts
@@ -0,0 +1,3 @@
+export const gomDev = ["React", "Next.js", "TypeScript", "Astro", "Docker", "Git", "MS Power Apps"];
+
+export const gomStudent = ["Node.js", "JavaScript", "Python", "MongoDB", "CRM"];
